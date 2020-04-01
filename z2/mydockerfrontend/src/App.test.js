1393d3f6c65d3b7d8b3486879dbb175df4daadd7
kaaspaa <kasper12.01@wp.pl>
2020-04-01 18:04:03 +0200

    finished zad2 questions

--- z2/mydockerfrontend/src/App.test.js
+++ z2/mydockerfrontend/src/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom'
 import App from './App';
 
-it('renders without crashing', () => {
+test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
